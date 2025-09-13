4c82ee15997ba75c1ed2d640ff5ad70513c0074e
miguelpanfilo <miguepanfilo@outlook.com>
2025-09-13 16:07:52 -0300

    Red: gestion de items/creacion item ok
    Test unitario del requerimiento gestion de items, creacion de item de forma correcta

---
+++ NearDupFinder/Test_gestion_de_items/UnitTest1.cs
@@ -0,0 +1,28 @@
+namespace Test_gestion_de_items;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestItems_CrearItemOk()
+    {
+        Catalogo catalogo = new Catalogo
+        {
+            Titulo = "Titulo de catalogo"
+        };
+        Item i = new Item
+        {
+            Titulo = "Soy un titulo",
+            Descripcion = "Soy una descripcion",
+            Catalogo = catalogo
+        };
+
+        
+        Assert.AreEqual("Soy un titulo", i.Titulo);
+        Assert.AreEqual("Soy una descripcion", i.Descripcion);
+        Assert.IsNotNull(i.Catalogo);
+        Assert.AreEqual("Titulo de catalogo", i.Catalogo.Titulo);
+            
+
+    }
+}
