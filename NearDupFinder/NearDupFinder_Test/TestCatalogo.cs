792162d5919becc41dc916f082c4af063486a194
marianoapn <marianoapn@gmail.com>
2025-09-14 15:48:17 -0300

    Test(Titulo): ROJO - CrearCatalogo_OkTest

---
+++ NearDupFinder/NearDupFinder_Test/TestCatalogo.cs
@@ -0,0 +1,14 @@
+
+namespace NearDupFinder_Test;
+
+[TestClass]
+public class CatalogoTest
+{
+    [TestMethod]
+    public void CrearCatalogo_OkTest()
+    {
+        Catalogo c = new Catalogo("Stock Tata");
+		
+        Assert.AreEqual("Stock Tata", c.Titulo);
+    }
+}
