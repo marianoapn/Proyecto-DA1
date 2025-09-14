1ba15a7a787aef79117f22273d85d272c06b9f0f
marianoapn <marianoapn@gmail.com>
2025-09-14 17:16:29 -0300

    Test(Descripcion): RED -  CambiarDescripcion_OkTest

--- NearDupFinder/NearDupFinder_Test/TestCatalogo.cs
+++ NearDupFinder/NearDupFinder_Test/TestCatalogo.cs
@@ -20,5 +20,13 @@ public class CatalogoTest
         
         Assert.AreEqual("El titulo es obligatorio", ex.Message);
     }
-
+    
+    [TestMethod]
+    public void CambiarDescripcion_OkTest()
+    {
+        Catalogo c = new Catalogo("Stock Tata");
+        c.CambiarDescripcion("Local numero 145, del barrio Centro");
+        Assert.AreEqual("Local numero 145, del barrio Centro", c.Descripcion);
+    }
+    
 }
