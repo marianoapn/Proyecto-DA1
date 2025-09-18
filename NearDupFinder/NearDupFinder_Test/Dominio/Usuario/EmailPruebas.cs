544a97789ce45ff8d724a2c7d529aa22f5bb0fb5
ManuPerez <157925331+ManuPerez182@users.noreply.github.com>
2025-09-18 11:07:07 -0300

    Test Email: RED - Crear_FormatoInvalido_LanzaExcepcion
    Creacion del test Crear_FormatoInvalido_LanzaExcepcion

--- NearDupFinder/NearDupFinder_Test/Dominio/Usuario/EmailPruebas.cs
+++ NearDupFinder/NearDupFinder_Test/Dominio/Usuario/EmailPruebas.cs
@@ -19,4 +19,18 @@ public class EmailPruebas
         Assert.IsNotNull(email);
         Assert.AreEqual(emailEsperado, email.ToString());
     }
+
+    [DataTestMethod]
+    [DataRow("")]
+    [DataRow("usuario@")]
+    [DataRow("@ejemplo.com")]
+    [DataRow("usuarioejemplo.com")]
+    [DataRow("usuario@ejemplo")]
+    [DataRow("usuario@.com")]
+    [DataRow("usuario@ejemplo.")]
+    [DataRow("usuario@ejemplo..com")]
+    public void Crear_Invalido_LanzaExcepcion(string textoCorreoInvalido)
+    {
+        Assert.ThrowsException<ArgumentException>(() => Email.Crear(textoCorreoInvalido));
+    }
 }
