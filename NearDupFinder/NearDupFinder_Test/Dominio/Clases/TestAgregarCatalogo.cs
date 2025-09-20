cdcb07c60f23ed0ed3aba55fcf84e3d377a6bd3b
marianoapn <marianoapn@gmail.com>
2025-09-20 18:54:48 -0300

    Test(AgregarCatalogo) Red - AgregarCatalogo_OkTest

---
+++ NearDupFinder/NearDupFinder_Test/Dominio/Clases/TestAgregarCatalogo.cs
@@ -0,0 +1,19 @@
+using NearDupFinder_Dominio;
+
+namespace NearDupFinder_Test.Dominio.Clases;
+
+[TestClass]
+public class TestAgregarCatalogo
+{   
+    [TestMethod]
+    public void AgregarCatalogo_OkTest()
+    {
+        Sistema s = new Sistema();
+        Catalogo c = new Catalogo("Catálogo");
+        
+        s.AgregarCatalogo(c);
+        
+        Assert.AreEqual(1, s.CantidadDeCatalogos());
+        
+    }
+}
