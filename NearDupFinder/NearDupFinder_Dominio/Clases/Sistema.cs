82f6170c30e2dd439cba28edb76c13c22fd64c11
marianoapn <marianoapn@gmail.com>
2025-09-22 21:24:57 -0300

    refactor: unificar Sistema.cs dentro de Clases y ajustar namespace duplicado

--- NearDupFinder/NearDupFinder_Dominio/Clases/Sistema.cs
+++ NearDupFinder/NearDupFinder_Dominio/Clases/Sistema.cs
@@ -3,12 +3,15 @@ namespace NearDupFinder_Dominio.Clases;
 public class Sistema
 {
     private readonly List<Catalogo> _catalogos;
+    private readonly List<Usuario> _usuarios = [];
 
     public Sistema()
     {
         _catalogos = new List<Catalogo>();
+        _usuarios.Add(CrearUsuarioAdmin());
     }
-
+    
+    /* Comienzo espacio Catalogo*/
     public void AgregarCatalogo(Catalogo c)
     {
         if (c == null)
@@ -30,4 +33,39 @@ public class Sistema
     {
         return _catalogos.Count;
     }
+    /* Fin espacio Catalogo*/
+    
+    /* Comienzo espacio Usuario*/
+    private Usuario CrearUsuarioAdmin()
+    {
+        Email email = Email.Crear("admin@gmail.com");
+        Fecha fecha = Fecha.Crear(1997,12,27);
+        Usuario adminUsuario = Usuario.Crear("admin","admin",email,fecha);
+        adminUsuario.AgregarRol(Rol.Administrador);
+        Contrasena contrasena = Contrasena.Crear("123QWEasdzxc@");
+        adminUsuario.CambiarContrasena(contrasena);
+        
+        return adminUsuario;
+    }
+
+    public Usuario? AutenticarUsuario(string? email, string? clave)
+    {
+        Email emailAValidar;
+        try
+        {
+            emailAValidar = Email.Crear(email);
+        }
+        catch
+        {
+            return null;
+        }
+
+        foreach (var usuario in _usuarios)
+        {
+            if (usuario.Email.Igual(emailAValidar) && usuario.VerificarContrasena(clave))
+                return usuario;
+        }
+        return null;
+    }
+    /*Fin espacio usuario*/
 }
