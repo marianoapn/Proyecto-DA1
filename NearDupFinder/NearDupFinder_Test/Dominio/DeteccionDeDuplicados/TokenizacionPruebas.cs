4659d669f3c2b19eca5811664d07c9bbe8c8ee2d
marianoapn <marianoapn@gmail.com>
2025-09-23 21:10:17 -0300

    Test(tokenizacion): Green - CrearToken_TituloConEspaciosDeMas_Ok

--- NearDupFinder/NearDupFinder_Test/Dominio/DeteccionDeDuplicados/TokenizacionPruebas.cs
+++ NearDupFinder/NearDupFinder_Test/Dominio/DeteccionDeDuplicados/TokenizacionPruebas.cs
@@ -23,6 +23,26 @@ public class TokenizacionPruebas
             new[] { "Celular", "de", "última", "generación" },
             tokens.TokenDescripcion
         );
-
     }
+    
+    [TestMethod]
+    public void CrearToken_TituloConEspaciosDeMas_Ok()
+    {
+        Sistema _sistema = new Sistema();
+        Item item = new Item("Iphone   17", "Celular de última generación");
+        
+        ItemTokenizado tokens = _sistema.TokenizarItem(item);
+        
+        CollectionAssert.AreEqual(
+            new[] { "Iphone", "17" },
+            tokens.TokenTitulo
+        );
+
+        CollectionAssert.AreEqual(
+            new[] { "Celular", "de", "última", "generación" },
+            tokens.TokenDescripcion
+        );
+    }
+    
+
 }
