d678d83fc469355431072cdbbd656e9403f7de8d
marianoapn <marianoapn@gmail.com>
2025-09-23 21:29:06 -0300

    Test(tokenizacion): Refactor

--- NearDupFinder/NearDupFinder_Test/Dominio/DeteccionDeDuplicados/TokenizacionPruebas.cs
+++ NearDupFinder/NearDupFinder_Test/Dominio/DeteccionDeDuplicados/TokenizacionPruebas.cs
@@ -6,10 +6,21 @@ namespace NearDupFinder_Test.Dominio.DeteccionDeDuplicados;
 [TestClass]
 public class TokenizacionPruebas
 {
+    private Sistema _sistema = null!;
+    [TestInitialize]
+    public void Setup()
+    {
+        _sistema = new Sistema();
+    }
+    [TestCleanup]
+    public void TearDown()
+    {
+        _sistema = null!; 
+    }
+    
     [TestMethod]
     public void CrearToken_Ok()
     {
-        Sistema _sistema = new Sistema();
         Item item = new Item("Iphone 17", "Celular de última generación");
         
         ItemTokenizado tokens = _sistema.TokenizarItem(item);
@@ -28,7 +39,6 @@ public class TokenizacionPruebas
     [TestMethod]
     public void CrearToken_TituloConEspaciosDeMas_Ok()
     {
-        Sistema _sistema = new Sistema();
         Item item = new Item("Iphone   17", "Celular de última generación");
         
         ItemTokenizado tokens = _sistema.TokenizarItem(item);
@@ -46,10 +56,10 @@ public class TokenizacionPruebas
     [TestMethod]
     public void CrearToken_TextoConNumeros_Ok()
     {
-        var s = new Sistema();
+
         var item = new Item("ps5 slim 1tb", "ssd 512");
 
-        var tokens = s.TokenizarItem(item);
+        var tokens = _sistema.TokenizarItem(item);
 
         CollectionAssert.AreEqual(new[] { "ps5", "slim", "1tb" }, tokens.TokenTitulo);
         CollectionAssert.AreEqual(new[] { "ssd", "512" }, tokens.TokenDescripcion);
@@ -58,10 +68,9 @@ public class TokenizacionPruebas
     [TestMethod]
     public void CrearToken_ConEspaciosExtremos_Ok()
     {
-        var s = new Sistema();
         var item = new Item("   iphone 17   ", "   celular   de   ultima   generacion   ");
 
-        var tokens = s.TokenizarItem(item);
+        var tokens = _sistema.TokenizarItem(item);
 
         CollectionAssert.AreEqual(new[] { "iphone", "17" }, tokens.TokenTitulo);
         CollectionAssert.AreEqual(new[] { "celular", "de", "ultima", "generacion" }, tokens.TokenDescripcion);
@@ -70,8 +79,7 @@ public class TokenizacionPruebas
     [TestMethod]
     public void TokenizarItem_ItemNull_TiraArgumentNull()
     {
-        var s = new Sistema();
-        Assert.ThrowsException<ArgumentNullException>(() => s.TokenizarItem(null));
+        Assert.ThrowsException<ArgumentNullException>(() => _sistema.TokenizarItem(null));
     }
     
 }
