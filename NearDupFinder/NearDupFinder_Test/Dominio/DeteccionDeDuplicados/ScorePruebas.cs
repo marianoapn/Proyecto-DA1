e30b759d8271e6aa9f44aa61fa0b7761d0780445
ManuPerez <157925331+ManuPerez182@users.noreply.github.com>
2025-09-26 18:22:37 -0300

    Test Score: RED - CalcularScore_JaccardTitulo_MayorAUno_LanzaArgumentOutOfRangeException

--- NearDupFinder/NearDupFinder_Test/Dominio/DeteccionDeDuplicados/ScorePruebas.cs
+++ NearDupFinder/NearDupFinder_Test/Dominio/DeteccionDeDuplicados/ScorePruebas.cs
@@ -120,4 +120,16 @@ public class ScorePruebas
 
         Assert.ThrowsException<ArgumentOutOfRangeException>(() => _sistema.CalcularScore(jaccardTitulo, jaccardDescripcion, marcaEq, modeloEq));
     }
+    
+    [TestMethod]
+    public void CalcularScore_JaccardTitulo_MayorAUno_LanzaArgumentOutOfRangeException()
+    {
+        float jaccardTitulo = 2.0f;
+        float jaccardDescripcion = 0f;
+        float marcaEq = 0f;
+        float modeloEq = 0f;
+
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => _sistema.CalcularScore(jaccardTitulo, jaccardDescripcion, marcaEq, modeloEq));
+    }
+
 }
