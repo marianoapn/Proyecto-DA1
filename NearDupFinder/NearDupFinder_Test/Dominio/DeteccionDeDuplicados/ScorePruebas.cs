94fd70bc1f8361f9848d7029243c4e2fcf486c98
ManuPerez <157925331+ManuPerez182@users.noreply.github.com>
2025-09-26 17:50:15 -0300

    Test Score: GREEN - CalcularScore_SoloModeloUno_Retorna0_10

--- NearDupFinder/NearDupFinder_Test/Dominio/DeteccionDeDuplicados/ScorePruebas.cs
+++ NearDupFinder/NearDupFinder_Test/Dominio/DeteccionDeDuplicados/ScorePruebas.cs
@@ -57,4 +57,17 @@ public class ScorePruebas
         
         Assert.AreEqual(score, 0.10f);
     }
+
+    [TestMethod]
+    public void CalcularScore_SoloModeloUno_Retorna0_10()
+    {
+        float jaccardTitulo = 0;
+        float jaccardDescripcion = 0;
+        float marcaEq = 0;
+        float modeloEq = 1f;
+        
+        float score = _sistema.CalcularScore(jaccardTitulo, jaccardDescripcion, marcaEq,  modeloEq);
+        
+        Assert.AreEqual(score, 0.10f);
+    }
 }
