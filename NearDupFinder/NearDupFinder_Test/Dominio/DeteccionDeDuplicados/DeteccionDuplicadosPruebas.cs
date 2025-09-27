ccffdc3d340cff331045b6be2e3f18181aef1238
ManuPerez <157925331+ManuPerez182@users.noreply.github.com>
2025-09-27 20:56:12 -0300

    Test Detectar Duplicados: RED - DetectarDuplicados_ScoreMenor060_NoAgrega

--- NearDupFinder/NearDupFinder_Test/Dominio/DeteccionDeDuplicados/DeteccionDuplicadosPruebas.cs
+++ NearDupFinder/NearDupFinder_Test/Dominio/DeteccionDeDuplicados/DeteccionDuplicadosPruebas.cs
@@ -37,4 +37,34 @@ public class DeteccionDuplicadosPruebas
         Assert.AreEqual(1, duplicados.Count);
         Assert.AreEqual(itemB.Id, duplicados[0].ItemB.Id);
     }
+    
+    [TestMethod]
+    public void DetectarDuplicados_ScoreMenor060_NoAgrega()
+    {
+        Item itemA = new Item
+        {
+            Titulo = "Notebook Lenovo L14",
+            Descripcion = "Intel i5 8GB 256GB SSD",
+            Marca = "Lenovo",
+            Modelo = "L14",
+            Categoria = "Notebooks"
+        };
+
+        Item itemC = new Item
+        {
+            Titulo = "Cafetera Espresso 15 bar",
+            Descripcion = "bomba italiana metalica",
+            Marca = "Philips",
+            Modelo = "EP1220",
+            Categoria = "Cocina"
+        };
+
+        Catalogo catalogo = new Catalogo("Catalogo");
+        catalogo.AgregarItem(itemA);
+        catalogo.AgregarItem(itemC);
+
+        List<Duplicados> duplicados = _sis.DetectarDuplicados(itemA, catalogo);
+
+        Assert.AreEqual(0, duplicados.Count);
+    }
 }
