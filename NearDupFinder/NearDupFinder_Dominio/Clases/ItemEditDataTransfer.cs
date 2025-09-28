9204a96d02af1f3eeb3db2fb9be2c539587e80d3
miguelpanfilo <miguepanfilo@outlook.com>
2025-09-28 11:03:13 -0300

    Test(ItemEditDataTransfer):VERDE - Editar modelo de un item

--- NearDupFinder/NearDupFinder_Dominio/Clases/ItemEditDataTransfer.cs
+++ NearDupFinder/NearDupFinder_Dominio/Clases/ItemEditDataTransfer.cs
@@ -1,6 +1,10 @@
 namespace NearDupFinder_Dominio.Clases;
 public class ItemEditDataTransfer
 {
+    public int Id { get; set; }             
     public string Titulo { get; set; }
     public string Descripcion { get; set; }
+    public string Categoria { get; set; }
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
 }
