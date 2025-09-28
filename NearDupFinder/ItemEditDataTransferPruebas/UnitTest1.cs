f56f8f9b429655be795c0e8302ecbd0338751920
miguelpanfilo <miguepanfilo@outlook.com>
2025-09-28 11:02:28 -0300

    Test(ItemEditDataTransfer):RED- Editar modelo de un item

--- NearDupFinder/ItemEditDataTransferPruebas/UnitTest1.cs
+++ NearDupFinder/ItemEditDataTransferPruebas/UnitTest1.cs
@@ -7,7 +7,7 @@ public class ListaCRUDTests
 {
     [TestMethod]
     public void CrearItemEditDataTransfer_ConPropiedades()
-    {
+    { 
         var itemEdit = new ItemEditDataTransfer();
 
         
@@ -16,5 +16,26 @@ public class ListaCRUDTests
 
         Assert.AreEqual("Mi Titulo", itemEdit.Titulo);
         Assert.AreEqual("Mi descripcion", itemEdit.Descripcion);
+    }
+
+    [TestMethod]
+    public void ItemEditDataTransfer_PuedeAlmacenarTodasLasPropiedades()
+    {
+        var itemEdit = new ItemEditDataTransfer();
+
+        itemEdit.Id = 10;
+        itemEdit.Titulo = "Titulo";
+        itemEdit.Descripcion = "Descripcion";
+        itemEdit.Categoria = "Categoria1";
+        itemEdit.Marca = "MarcaX";
+        itemEdit.Modelo = "ModeloY";
+
+        
+        Assert.AreEqual(10, itemEdit.Id);
+        Assert.AreEqual("Titulo", itemEdit.Titulo);
+        Assert.AreEqual("Descripcion", itemEdit.Descripcion);
+        Assert.AreEqual("Categoria1", itemEdit.Categoria);
+        Assert.AreEqual("MarcaX", itemEdit.Marca);
+        Assert.AreEqual("ModeloY", itemEdit.Modelo);
     }
 }
