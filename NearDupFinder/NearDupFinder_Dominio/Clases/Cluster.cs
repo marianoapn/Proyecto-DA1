eb7cb89590a9a25c4ae7ca4ed38134fab61bc60a
marianoapn <marianoapn@gmail.com>
2025-10-05 21:06:37 -0300

    Test(clusters): Green
    Cluster_Canonico_EsElDeDescripcionMasLarga

--- NearDupFinder/NearDupFinder_Dominio/Clases/Cluster.cs
+++ NearDupFinder/NearDupFinder_Dominio/Clases/Cluster.cs
@@ -4,17 +4,27 @@ public class Cluster
 {
     public int Id { get; }
     private readonly HashSet<Item> _pertenecientesCluster;
-
+    public Item Canonico { get; private set; }
     public  IEnumerable<Item> PertenecientesCluster => _pertenecientesCluster;
 
     public  Cluster(int id, HashSet<Item> pertenecientesCluster)
     {
         Id = id;
         _pertenecientesCluster = pertenecientesCluster;
+        ActualizarCanonico();
     }
     
     public void Agregar(Item item) => _pertenecientesCluster.Add(item);
     
     public void Remover(Item item) => _pertenecientesCluster.Remove(item);
     public bool Contiene(Item item) => _pertenecientesCluster.Contains(item);
+
+    private void ActualizarCanonico()
+    {
+        Canonico = _pertenecientesCluster
+            .OrderByDescending(i => i.Descripcion.Length)
+            .ThenByDescending(i => i.Titulo.Length)
+            .ThenBy(i => i.Id)
+            .First();
+    }
 }
