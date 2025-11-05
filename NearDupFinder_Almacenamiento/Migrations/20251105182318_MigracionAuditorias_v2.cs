f346d8f12273daa091d8d7914789f4ccd5b600d0
miguelpanfilo <miguepanfilo@outlook.com>
2025-11-05 15:28:48 -0300

    implementacion en el compose de la tabla de Auditorias
    Creacion de la tabla con las columnas correctas de la entidad auditoria.

---
+++ NearDupFinder_Almacenamiento/Migrations/20251105182318_MigracionAuditorias_v2.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NearDupFinder_Almacenamiento.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionAuditorias_v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Usuario",
+                table: "Auditorias",
+                type: "nvarchar(320)",
+                maxLength: 320,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(120)",
+                oldMaxLength: 120);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Detalles",
+                table: "Auditorias",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Usuario",
+                table: "Auditorias",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(320)",
+                oldMaxLength: 320);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Detalles",
+                table: "Auditorias",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+        }
+    }
+}
