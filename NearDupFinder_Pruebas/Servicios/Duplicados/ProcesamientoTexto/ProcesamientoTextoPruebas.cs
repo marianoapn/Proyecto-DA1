6280db2d8ccf37960b972be05fd201c1d49dac3e
miguelpanfilo <miguepanfilo@outlook.com>
2025-11-05 10:31:00 -0300

    Test(StopwordStemming):RED - Creacion de interfaz procesador texto

---
+++ NearDupFinder_Pruebas/Servicios/Duplicados/ProcesamientoTexto/ProcesamientoTextoPruebas.cs
@@ -0,0 +1,29 @@
+
+
+namespace NearDupFinder_Pruebas.Servicios.Duplicados.ProcesamientoTexto
+{
+    [TestClass]
+    public class ProcesamientoTextoPruebas
+    {
+        private ProcesadorTexto _procesador = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _procesador = new ProcesadorTexto();
+        }
+
+        [TestMethod]
+        public void AplicarStopwords_TextoConStopwords_DeberiaEliminarSoloLasStopwords()
+        {
+            string[] entrada = { "el", "gato", "de", "la", "casa" };
+
+            string[] salida = _procesador.AplicarStopwords(entrada);
+
+            CollectionAssert.AreEquivalent(
+                new[] { "gato", "casa" },
+                salida
+            );
+        }
+    }
+}
