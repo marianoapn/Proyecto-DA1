d7e60298dd96359868f3cf67e4e8c1271f3d913d
miguelpanfilo <miguepanfilo@outlook.com>
2025-11-05 10:57:44 -0300

    Test(StopwordStemming):GREEN -Stemming implementacion de funcion

--- LogicaDeNegocios/NearDupFInder_LogicaDeNegocio/Servicios/Duplicados/ProcesamientoTexto/IProcesadorTexto.cs
+++ LogicaDeNegocios/NearDupFInder_LogicaDeNegocio/Servicios/Duplicados/ProcesamientoTexto/IProcesadorTexto.cs
@@ -3,5 +3,7 @@ namespace NearDupFinder_LogicaDeNegocio.Servicios.Duplicados.ProcesamientoTexto;
 public interface IProcesadorTexto
 { 
     string[] AplicarStopwords(string[] tokensEntrada);
+    string[] AplicarStemming(string[] tokensEntrada);
+
     
 }
--- LogicaDeNegocios/NearDupFInder_LogicaDeNegocio/Servicios/Duplicados/ProcesamientoTexto/ProcesadorTexto.cs
+++ LogicaDeNegocios/NearDupFInder_LogicaDeNegocio/Servicios/Duplicados/ProcesamientoTexto/ProcesadorTexto.cs
@@ -1,7 +1,12 @@
+using Lucene.Net.Tartarus.Snowball.Ext;
+using System.Collections.Generic;
+using System.Linq;
 namespace NearDupFinder_LogicaDeNegocio.Servicios.Duplicados.ProcesamientoTexto;
 
 public class ProcesadorTexto : IProcesadorTexto
 {
+    private readonly SpanishStemmer _stemmer = new();
+
     public string[] AplicarStopwords(string[] tokensEntrada)
     {
         if (tokensEntrada == null || tokensEntrada.Length == 0)
@@ -12,6 +17,24 @@ public class ProcesadorTexto : IProcesadorTexto
             .ToArray();
         
     }
+
+    public string[] AplicarStemming(string[] tokensEntrada)
+    {
+        if (tokensEntrada == null || tokensEntrada.Length == 0)
+            return Array.Empty<string>();
+
+        var resultado = new List<string>();
+
+        foreach (var token in tokensEntrada)
+        {
+            _stemmer.SetCurrent(token);
+            _stemmer.Stem();
+            resultado.Add(_stemmer.Current);
+        }
+
+        return resultado.Distinct().ToArray();
+    }
+
     private static readonly HashSet<string> _stopwords = new(StringComparer.OrdinalIgnoreCase)
     {
         "el", "la", "los", "las", "de", "y", "en", "un", "una", "para", "por"
